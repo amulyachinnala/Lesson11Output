3604607f2894c246965dd4d0899670c0ece94c5a
amulyachinnala <amulya.chinnala07@gmail.com>
2025-04-20 21:16:37 -0400

    Initial Commit

---
+++ Lesson11Output/Lesson11OutputApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson11OutputApp.swift
+//  Lesson11Output
+//
+//  Created by Amulya on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Lesson11OutputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lesson11OutputTests/Lesson11OutputTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson11OutputTests.swift
+//  Lesson11OutputTests
+//
+//  Created by Amulya on 4/20/25.
+//
+
+import Testing
+@testable import Lesson11Output
+
+struct Lesson11OutputTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
