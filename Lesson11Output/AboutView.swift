506b64550aaa6d94e5788aa011d28a5bfe06bbf2
amulyachinnala <amulya.chinnala07@gmail.com>
2025-04-29 08:49:50 -0400

    Created Home page (ContentView) with house icon and button to About page.

---
+++ Lesson11Output/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  Lesson11Output
+//
+//  Created by Amulya on 4/20/25.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
--- Lesson11Output/ContentView.swift
+++ Lesson11Output/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack {
+                NavigationLink(destination: AboutView()) {
+                    Image(systemName: "house.fill")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.red)
+                }
+                Text("Home Page")       .font(.title2)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 50)
+                NavigationLink(destination: AboutView()) {
+                    Text("Go to About Page")
+                }
+                .navigationTitle("Home")
+                .padding()
+                .background(Color.red)
+                .foregroundColor(.white)
+                .fontWeight(.bold)
+                .cornerRadius(10)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
