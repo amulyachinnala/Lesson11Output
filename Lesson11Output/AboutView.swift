d328aed83b5947bfac4344495fc91a101e3a2e53
amulyachinnala <amulya.chinnala07@gmail.com>
2025-04-29 08:54:38 -0400

    Created About page (AboutView) with info circle icon and button to Contact page.

--- Lesson11Output/AboutView.swift
+++ Lesson11Output/AboutView.swift
@@ -7,12 +7,34 @@
 
 import SwiftUI
 
-struct AboutView: View {
+struct FirstView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack{
+                NavigationLink(destination: ContactView()) {
+                    Image(systemName: "info.circle.fill")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.yellow)
+                }
+                Text("About Page")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 50)
+                NavigationLink(destination: ContactView()) {
+                    Text("Go to Contact Page")
+                }
+                .navigationTitle("About")
+                .padding()
+                .background(Color.yellow)
+                .foregroundColor(.white)
+                .fontWeight(.bold)
+                .cornerRadius(10)
+            }
+        }
     }
 }
 
 #Preview {
-    AboutView()
+    FirstView()
 }
---
+++ Lesson11Output/ContactView.swift
@@ -0,0 +1,18 @@
+//
+//  ContactView.swift
+//  Lesson11Output
+//
+//  Created by Amulya on 4/29/25.
+//
+
+import SwiftUI
+
+struct ContactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContactView()
+}
